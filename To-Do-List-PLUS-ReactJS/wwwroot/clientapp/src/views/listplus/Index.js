352e360042e7c84ddba25e88a8c2ee6dd47905e0
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2022-05-12 15:03:12 -0300

    Update 12May 15:03
    All functions completed! add, delete, edit, and search.
    Search button removed.

--- To-Do-List-PLUS-ReactJS/wwwroot/clientapp/src/views/listplus/Index.js
+++ To-Do-List-PLUS-ReactJS/wwwroot/clientapp/src/views/listplus/Index.js
@@ -17,7 +17,6 @@ export default class Index
             indexEdicao: -1,
             filter: ""
         }
-
     }
 
     add = () => {
@@ -29,26 +28,30 @@ export default class Index
             tasksAux[this.state.indexEdicao] = this.state.item;
 
         this.setState ({
+            ...this.state,
             indexEdicao: -1,
             item: {
                 id: "",
                 task: ""
             },
             tasks: tasksAux,
-            filter: "",
         });
     }
 
     delete = (i) => {
+        if (!confirm(`Do you really want to delete the task: ${i.task}?`))
+            return;   
+
         let tasksAux = this.state.tasks;
         let index = -1;
         
         for(let pos = 0;pos < tasksAux.length;pos++)
-            if(tasksAux[pos].task == i.task)
+            if(tasksAux[pos].id == i.id)
                 index = pos;
         if(index > -1) {
             tasksAux.splice(index, 1)
             this.setState ({
+                ...this.state,
                 tasks: tasksAux
             })
         }
@@ -56,31 +59,33 @@ export default class Index
 
     edit = (i) => {
         let tasksAux = this.state.tasks;
-        let index = tasksAux.findIndex(task => task == i)
+        let index = tasksAux.findIndex(item => item.id == i.id)
 
         if(index > -1)
         {
             let taskAux = tasksAux[index];
             this.setState ({
-                item: {
-                    id: taskAux.id,
-                    task: taskAux.task,
-                },
+                ...this.state,
+                item: taskAux,
                 indexEdicao: index
             }); 
         }
     }
 
-    search = () => { // Not working
+    search = () => {
+        let filter = this.state.filter;
         let tasksAux = this.state.tasks;
 
-        for(let pos = 0;pos < tasksAux.length;pos++)
-            if(tasksAux[pos].task !== filter) {
-                tasksAux.splice(pos, 1)
-                this.setState ({
-                    vetAux: tasksAux,
-                });
-            }       
+        tasksAux.forEach(i => {
+            if(i.task.indexOf(filter) > -1)
+                i.filtered = true;
+            else i.filtered = false;
+        });
+
+        this.setState({
+            ...this.state,
+            tasks: tasksAux
+        })    
     }
 
     render = () => {
@@ -89,33 +94,36 @@ export default class Index
                 <div class="main-box">
                     <h1>React - To-do list PLUS</h1>
                     <div class="form">                       
-                        <input type="text" placeholder="ID here" value={this.state.item.id} onChange={(e) => this.setState({ item: { id: e.target.value, task: this.state.item.task} })}/>
-                        <input type="text"  placeholder="Task here" value={this.state.item.task} onChange={(e) => this.setState({ item: { id: this.state.item.id, task: e.target.value} })}/>
+                        <input type="text" placeholder="ID here" value={this.state.item.id} onChange={(e) => this.setState({ ...this.state, item: { ...this.state.item, id: e.target.value } })}/>
+                        <input type="text"  placeholder="Task here" value={this.state.item.task} onChange={(e) => this.setState({ ...this.state, item: { ...this.state.item, task: e.target.value } })}/>
                         <button class="button-add" type="button" onClick={() => this.add()}>Add</button>
                     </div>
                     <div>
-                        <input class="search-input" placeholder="Type task here to search" type="text" value={this.state.filter} onChange={(e) => this.setState({ filter: e.target.value })}/>
+                        <input class="search-input" placeholder="Type task here to search" type="search" value={this.state.filter} onChange={(e) => this.setState({ ...this.state, filter: e.target.value }, () => { this.search() } )}/>
                     </div>            
-                    <button type="button" onClick={() => this.search()}>Search</button>
                     <br />
                     <br />
                 </div>
                     <table class="list-search">
                     {         
-                        this.state.tasks.map(i => { return (
-                            <tr>
-                                <td class="search-result">{i.id}</td> 
-                                <td class="search-result">{i.task}</td>
-                                <div class="search-buttons">
-                                    <td>
-                                        <button class="search-button-delete" type="button" onClick={() => this.delete(i)}>Delete</button>
-                                    </td>
-                                    <td>
-                                        <button class="search-button-edit" type="button" onClick={() => this.edit(i)}>Edit</button>
-                                    </td>
-                                </div>
-                            </tr>
-                        )})
+                        this.state.tasks.map(i => { 
+                            if (i.filtered == undefined || i.filtered)
+                            {
+                                return (    
+                                    <tr>
+                                        <td class="search-result">{i.id + " - " + i.task}</td>
+                                        <div class="search-buttons">
+                                            <td>
+                                                <button class="search-button-delete" type="button" onClick={() => this.delete(i)}>Delete</button>
+                                            </td>
+                                            <td>
+                                                <button class="search-button-edit" type="button" onClick={() => this.edit(i)}>Edit</button>
+                                            </td>
+                                        </div>
+                                    </tr>
+                                )
+                            }                           
+                        })
                     }
                     </table>
             </>
